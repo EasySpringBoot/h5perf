1eed9fad7050394871e12d0c7b39b2c25fc09e2b
jack <guangjian.cgj@alibaba-inc.com>
2017-04-23 23:53:21 +0800

    属性配置

--- src/main/groovy/com/easy/springboot/h5perf/config/WebMvcConfig.groovy
+++ src/main/groovy/com/easy/springboot/h5perf/config/WebMvcConfig.groovy
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 /**
  * Created by jack on 2017/4/23.
+ * 有些灵活的配置，还是使用代码，更加灵活方便
  * http://docs.spring.io/spring-boot/docs/current/api/org/springframework/boot/autoconfigure/web/WebMvcAutoConfiguration.EnableWebMvcConfiguration.html
  */
 
@@ -16,7 +17,6 @@ class WebMvcConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/")
 
-
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/")
 
@@ -26,3 +26,11 @@ class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 
 }
+
+//#    mvc:
+//#    # 所有的访问都匹配映射到静态资源路径,spring.mvc.static-path-pattern 只可以定义一个，目前不支持多个逗号分割的方式
+//#        static-path-pattern: /**
+//#
+//#    resources:
+//#    # 这里的配置是覆盖默认配置，所以需要将默认的也加上否则static、public等这些路径将不能被当作静态资源路径
+//#        static-locations: classpath:/static/,classpath:/public/,classpath:/resources/,classpath:/META-INF/resources/,classpath:/META-INF/resources/webjars/
