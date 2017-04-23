27902eb8936bcc3c7e96720a1bd2397369dba94f
jack <guangjian.cgj@alibaba-inc.com>
2017-04-24 01:00:44 +0800

    groovy model 类，不用写setter，getter

--- src/main/groovy/com/easy/springboot/h5perf/model/TestCase.groovy
+++ src/main/groovy/com/easy/springboot/h5perf/model/TestCase.groovy
@@ -11,56 +11,8 @@ class TestCase {
     Date gmtCreated
     Date gmtModified
 
-    Integer getId() {
-        return id
-    }
-
-    void setId(Integer id) {
-        this.id = id
-    }
-
-    String getContainerType() {
-        return containerType
-    }
-
-    void setContainerType(String containerType) {
-        this.containerType = containerType
-    }
-
-    String getTestUrl() {
-        return testUrl
-    }
-
-    void setTestUrl(String testUrl) {
-        this.testUrl = testUrl
-    }
-
-    String getNetType() {
-        return netType
-    }
-
-    void setNetType(String netType) {
-        this.netType = netType
-    }
-
-    Date getGmtCreated() {
-        return gmtCreated
-    }
-
-    void setGmtCreated(Date gmtCreated) {
-        this.gmtCreated = gmtCreated
-    }
-
-    Date getGmtModified() {
-        return gmtModified
-    }
-
-    void setGmtModified(Date gmtModified) {
-        this.gmtModified = gmtModified
-    }
-
     @Override
-    public String toString() {
+    String toString() {
         return "TestCase{" +
                 "id=" + id +
                 ", containerType='" + containerType + '\'' +
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,72 +1,59 @@
-CREATE DATABASE  IF NOT EXISTS `h5perf` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS `h5perf`
+  DEFAULT CHARACTER SET utf8;
 USE `h5perf`;
--- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
---
--- Host: localhost    Database: h5perf
--- ------------------------------------------------------
--- Server version       5.7.11
-
---
--- Table structure for table `test_case`
---
 
 DROP TABLE IF EXISTS `test_case`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `test_case` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `container_type` varchar(45) DEFAULT NULL COMMENT '‘ORIGIN_WEBVIEW’,’UC_WEBVIEW’',
-  `test_url` varchar(45) DEFAULT NULL,
-  `net_type` varchar(45) DEFAULT NULL COMMENT '‘WIFI’,‘3G’,‘4G’',
-  `gmt_created` datetime DEFAULT NULL,
-  `gmt_modified` datetime DEFAULT NULL,
+  `id`             INT(11) NOT NULL AUTO_INCREMENT,
+  `container_type` VARCHAR(45)      DEFAULT NULL
+  COMMENT '‘ORIGIN_WEBVIEW’,’UC_WEBVIEW’',
+  `test_url`       VARCHAR(45)      DEFAULT NULL,
+  `net_type`       VARCHAR(45)      DEFAULT NULL
+  COMMENT '‘WIFI’,‘3G’,‘4G’',
+  `gmt_created`    DATETIME         DEFAULT NULL,
+  `gmt_modified`   DATETIME         DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-
-
---
--- Table structure for table `test_resource`
---
 
 DROP TABLE IF EXISTS `test_resource`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `test_resource` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `test_case_id` int(11) DEFAULT NULL,
-  `start_time` bigint(11) DEFAULT NULL,
-  `resource_url` varchar(500) DEFAULT NULL,
-  `resource_type` varchar(45) DEFAULT NULL COMMENT 'IMG,JS,CSS,OTHER',
-  `resource_time` bigint(11) DEFAULT NULL COMMENT 'onLoadResource资源时间戳',
-  `resource_size` bigint(11) DEFAULT NULL COMMENT '资源大小',
-  `gmt_created` datetime DEFAULT NULL,
+  `id`            INT(11) NOT NULL AUTO_INCREMENT,
+  `test_case_id`  INT(11)          DEFAULT NULL,
+  `start_time`    BIGINT(11)       DEFAULT NULL,
+  `resource_url`  VARCHAR(500)     DEFAULT NULL,
+  `resource_type` VARCHAR(45)      DEFAULT NULL
+  COMMENT 'IMG,JS,CSS,OTHER',
+  `resource_time` BIGINT(11)       DEFAULT NULL
+  COMMENT 'onLoadResource资源时间戳',
+  `resource_size` BIGINT(11)       DEFAULT NULL
+  COMMENT '资源大小',
+  `gmt_created`   DATETIME         DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-
-
---
--- Table structure for table `test_time_data`
---
 
 DROP TABLE IF EXISTS `test_time_data`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `test_time_data` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `test_case_id` int(11) DEFAULT NULL,
-  `on_recieve_title` varchar(45) DEFAULT NULL COMMENT '近似白屏时间',
-  `on_page_finished` varchar(45) DEFAULT NULL COMMENT '页面加载完成时间',
-  `dom_content_load` varchar(45) DEFAULT NULL COMMENT 'dom内容加载完成时间',
-  `load` varchar(45) DEFAULT NULL COMMENT '资源加载完成时间',
-  `gmt_created` datetime DEFAULT NULL,
+  `id`               INT(11) NOT NULL AUTO_INCREMENT,
+  `test_case_id`     INT(11)          DEFAULT NULL,
+  `on_recieve_title` VARCHAR(45)      DEFAULT NULL
+  COMMENT '近似白屏时间',
+  `on_page_finished` VARCHAR(45)      DEFAULT NULL
+  COMMENT '页面加载完成时间',
+  `dom_content_load` VARCHAR(45)      DEFAULT NULL
+  COMMENT 'dom内容加载完成时间',
+  `load`             VARCHAR(45)      DEFAULT NULL
+  COMMENT '资源加载完成时间',
+  `gmt_created`      DATETIME         DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-
-
--- Dump completed on 2017-04-22 15:42:54
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
